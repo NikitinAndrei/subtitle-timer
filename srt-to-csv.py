c672c1416153ffc105d5cbe32bed276a60fe4a15
NikitinAndrei <andrewnikitinn@mail.ru>
2023-01-08 14:35:09 +0300

    test

--- srt-to-csv.py
+++ srt-to-csv.py
@@ -1,6 +1,6 @@
 import csv, re, srt
 
-with open('Subsnaudios/taxi_subs_12.srt', 'r') as f:
+with open('Subsnaudios/taxi_subs_13.srt', 'r') as f:
     subs = srt.parse(f)
     for line in f:
         print(f)
