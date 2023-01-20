8a179c1de01124e4366398cf2e6b03dee4baae00
NikitinAndrei <andrewnikitinn@mail.ru>
2023-01-20 23:25:00 +0300

    stopped on indexing check

--- srt-to-csv.py
+++ srt-to-csv.py
@@ -6,6 +6,43 @@ import soundfile as sf
 from tqdm import tqdm
 
 
+def subs_check(path="D:\\Projects\\SubsTimer\\Subsnaudios\\"):
+    indexing = {}
+    mistakes = {'file name': None, 'time_stamp': None, 'indexing': []}
+    for name in os.listdir(path):
+        if '.srt' in name:
+            with open(f'{path}\{name}', 'r', encoding='utf-8') as f:
+                lines = ''.join(f.readlines())
+            correct_time = re.compile(r'\n?[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}')
+            incorrect_time = re.compile(r'\n?[0-9]+:[0-9]+:[0-9]+,[0-9]+')
+            subs_timestamps = re.findall(correct_time, lines)
+            subs_misformat = re.findall(incorrect_time, lines)
+
+            if len(subs_misformat) != len(subs_timestamps):
+                if not mistakes['file name']:
+                    mistakes['file name'] = [name]
+                    mistakes['time_stamp'] = []
+                else:
+                    mistakes['file name'].append(name)
+                    mistakes['time_stamp'].append([])
+                for i, n in enumerate(subs_misformat):
+                    if n != subs_timestamps[n]:
+                        mistakes['time_stamp'][-1].append(n)
+
+            starts = [i[1:] for i in correct_time if i.startswith('\n')]
+            finishes = [i for i in correct_time if not i.startswith('\n')]
+            starts = [parse_to_sec(i) for i in starts]
+            finishes = [parse_to_sec(i) for i in finishes]
+            time_in_secs = []
+
+            for i, n in enumerate(starts):
+                time_in_secs.append(starts[i])
+                time_in_secs.append(finishes[i])
+                if finishes[i - 1] - starts[i] > 0 and i > 2:
+                    raise Exception(f'Mistake in {name} on position {i + 1} with time {parse_to_mins(finishes[i - 1])}')
+                time_in_secs.append(1)
+
+
 def change_indexes(subfile: str, path="D:\\Projects\\SubsTimer\\Subsnaudios\\"):
     with open(path + subfile, 'r', encoding='utf-8') as f:
         data = f.readlines()
@@ -28,7 +65,7 @@ def read_timestamps(name: str, folder='/Subsnaudios'):
     path = os.getcwd()
     with open(f'{path + folder}\{name}', 'r', encoding='utf-8') as f:
         lines = ''.join(f.readlines())
-    start = re.compile(r'\n?[0-9]+:[0-9]{2}:[0-9]{2},[0-9]+')
+    start = re.compile(r'\n?[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]+')
     sub_start = re.findall(start, lines)
     starts = [i[1:] for i in sub_start if i.startswith('\n')]
     finishes = [i for i in sub_start if not i.startswith('\n')]
